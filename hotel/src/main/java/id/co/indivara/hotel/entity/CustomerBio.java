3a97c4fa383f0e5183586d5310986a626310b57b
thaufaniqbal <thaufaniqbal@gmail.com>
2023-06-14 11:40:31 +0700

    update

--- hotel/src/main/java/id/co/indivara/hotel/entity/CustomerBio.java
+++ hotel/src/main/java/id/co/indivara/hotel/entity/CustomerBio.java
@@ -21,6 +21,7 @@ public class CustomerBio {
     private String customerName;
     private String customerGender;
     private String customerAddress;
+    private String smooking;
     private Long customerNumber;
     private LocalDate customerBirthDate;
     private Long idCardNumber;
