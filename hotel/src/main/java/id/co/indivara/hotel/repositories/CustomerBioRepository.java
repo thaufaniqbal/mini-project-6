4530f60f4d8c48b4b481de0f4d584ff285b73b5d
thaufaniqbal <thaufaniqbal@gmail.com>
2023-06-15 19:17:21 +0700

    update by development

---
+++ hotel/src/main/java/id/co/indivara/hotel/repositories/CustomerBioRepository.java
@@ -0,0 +1,7 @@
+package id.co.indivara.hotel.repositories;
+
+import id.co.indivara.hotel.entity.CustomerBio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerBioRepository extends JpaRepository<CustomerBio, Long> {
+}
